ab954e9a00b8fc4dc365f0c289b41512f0ae2372
Glen <glencbz@gmail.com>
2016-01-08 22:34:02 +0800

    Update 2016-01-08T14:33:44.220Z

--- js/build/main.js
+++ js/build/main.js
@@ -1,75 +1,158 @@
-$(function () {
-	var Pointer = React.createClass({
-		displayName: 'Pointer',
+var Backing = React.createClass({
+	displayName: 'Backing',
 
-		render: function () {
-			document.addEventListener('mouseup', this.onMouseUp);
-			document.addEventListener('mousedown', this.onMouseDown);
-			document.addEventListener('touchstart', this.onMouseDown);
-			document.addEventListener('touchend', this.onMouseUp);
-			return React.createElement(
+	render: function () {
+		document.addEventListener('mouseup', this.onMouseUp);
+		document.addEventListener('mousedown', this.onMouseDown);
+		document.addEventListener('touchstart', this.onMouseDown);
+		document.addEventListener('touchend', this.onMouseUp);
+		return React.createElement(
+			'div',
+			{ className: 'backing' },
+			React.createElement(
 				'div',
-				{ className: 'pointer', style: this.style() },
-				React.createElement('div', { className: 'head' }),
-				React.createElement('div', { className: 'body' }),
-				React.createElement('div', { className: 'tail' }),
-				React.createElement('div', { className: 'pin' })
-			);
-		},
-		getInitialState: function () {
-			return {
-				rotation: 0,
-				startMouseX: NaN,
-				startMouseY: NaN,
-				endMouseX: NaN,
-				endMouseY: NaN,
-				timeStart: Date.now(),
-				timeEnd: Date.now()
-			};
-		},
-		rotate: function (rotateAmount) {
-			var finalPosition = this.state.rotation + rotateAmount;
-			this.setState({ rotation: finalPosition });
-		},
-		style: function () {
-			return {
-				transform: 'rotate(' + this.state.rotation + 'deg)'
-			};
-		},
-		onMouseUp: function (e) {
-			if (e.type == "touchend") {
-				this.setState({
-					endMouseX: e.changedTouches[0].pageX,
-					endMouseY: e.changedTouches[0].pageY,
-					timeEnd: Date.now()
-				});
-			} else this.setState({
-				endMouseX: e.pageX,
-				endMouseY: e.pageY,
+				{ className: 'pure-g' },
+				React.createElement(
+					'div',
+					{ className: 'section top-left pure-u-1-2' },
+					React.createElement(
+						'div',
+						{ className: 'color-circle' },
+						React.createElement('i', { className: 'fa-arrow-down fa' })
+					)
+				),
+				React.createElement(
+					'div',
+					{ className: 'section top-right pure-u-1-2' },
+					React.createElement(
+						'div',
+						{ className: 'color-circle' },
+						React.createElement('i', { className: 'fa-arrow-down fa' })
+					)
+				)
+			),
+			React.createElement(
+				'div',
+				{ className: 'pure-g' },
+				React.createElement(
+					'div',
+					{ className: 'section bot-left pure-u-1-2' },
+					React.createElement(
+						'div',
+						{ className: 'color-circle' },
+						React.createElement('i', { className: 'fa-arrow-down fa' })
+					)
+				),
+				React.createElement(
+					'div',
+					{ className: 'section bot-right pure-u-1-2' },
+					React.createElement(
+						'div',
+						{ className: 'color-circle' },
+						React.createElement('i', { className: 'fa-arrow-down fa' })
+					)
+				)
+			),
+			React.createElement(Pointer, { rotation: this.state.rotation })
+		);
+	},
+	onMouseUp: function (e) {
+		if (e.type == "touchend") {
+			this.setState({
+				endMouseX: e.changedTouches[0].pageX,
+				endMouseY: e.changedTouches[0].pageY,
 				timeEnd: Date.now()
 			});
-			console.log(this.state);
-			var distance = dist(this.state.endMouseX, this.state.startMouseX, this.state.endMouseY, this.state.startMouseY);
-			var speed = getSpeed(distance, this.state.timeStart, this.state.timeEnd);
-			this.rotate(speedToRotation(speed));
-		},
-		onMouseDown: function (e) {
-			if (e.type == "touchstart") {
-				this.setState({
-					startMouseX: e.changedTouches[0].pageX,
-					startMouseY: e.changedTouches[0].pageY,
-					timeStart: Date.now()
-				});
-			} else this.setState({
-				startMouseX: e.pageX,
-				startMouseY: e.pageY,
+		} else this.setState({
+			endMouseX: e.pageX,
+			endMouseY: e.pageY,
+			timeEnd: Date.now()
+		});
+		var distance = dist(this.state.endMouseX, this.state.startMouseX, this.state.endMouseY, this.state.startMouseY);
+		var speed = getSpeed(distance, this.state.timeStart, this.state.timeEnd);
+		this.rotate(speedToRotation(speed));
+	},
+	onMouseDown: function (e) {
+		if (e.type == "touchstart") {
+			this.setState({
+				startMouseX: e.changedTouches[0].pageX,
+				startMouseY: e.changedTouches[0].pageY,
 				timeStart: Date.now()
 			});
-		}
-	});
+		} else this.setState({
+			startMouseX: e.pageX,
+			startMouseY: e.pageY,
+			timeStart: Date.now()
+		});
+	},
+	getInitialState: function () {
+		return {
+			rotation: 0,
+			startMouseX: NaN,
+			startMouseY: NaN,
+			endMouseX: NaN,
+			endMouseY: NaN,
+			timeStart: Date.now(),
+			timeEnd: Date.now()
+		};
+	},
+	rotate: function (rotateAmount) {
+		var finalPosition = this.state.rotation + rotateAmount;
+		this.setState({ rotation: finalPosition });
+	}
+});
+var Pointer = React.createClass({
+	displayName: 'Pointer',
 
-	var reactPointer = React.createElement(Pointer, null);
-	ReactDOM.render(reactPointer, document.getElementById("pointer"));
+	render: function () {
+		return React.createElement(
+			'div',
+			{ className: 'pointer', style: this.style() },
+			React.createElement('div', { className: 'head' }),
+			React.createElement('div', { className: 'body' }),
+			React.createElement('div', { className: 'tail' }),
+			React.createElement('div', { className: 'pin' })
+		);
+	},
+	style: function () {
+		return {
+			transform: 'rotate(' + this.props.rotation + 'deg)'
+		};
+	}
+});
+
+var reactBacking = React.createElement(Backing, null);
+ReactDOM.render(reactBacking, document.getElementById("backing"));
+
+var currentRotation = 0;
+
+$(function () {
+	var $pointer = $(".pointer");
+	var $topLeft = $(".top-left .fa");
+	var $topRight = $(".top-right .fa");
+	var $bottomRight = $(".bot-right .fa");
+	var $bottomLeft = $(".bot-left .fa");
+
+	var timer = setInterval(function () {
+		currentRotation = getRotationAngle($pointer.css("transform"));
+		console.log(currentRotation);
+		if (currentRotation >= 0 && currentRotation < 90) {
+			$topRight.css("display", "inline-block");
+			$topRight.css("transform", "rotate(" + currentRotation * 4 + "deg)");
+		} else $topRight.css("display", "none");
+		if (currentRotation >= 90 && currentRotation < 180) {
+			$bottomRight.css("display", "inline-block");
+			$bottomRight.css("transform", "rotate(" + (currentRotation - 90) * 4 + "deg)");
+		} else $bottomRight.css("display", "none");
+		if (currentRotation >= -180 && currentRotation < -90) {
+			$bottomLeft.css("display", "inline-block");
+			$bottomLeft.css("transform", "rotate(" + (currentRotation + 180) * 4 + "deg)");
+		} else $bottomLeft.css("display", "none");
+		if (currentRotation >= -90 && currentRotation < 0) {
+			$topLeft.css("display", "inline-block");
+			$topLeft.css("transform", "rotate(" + (currentRotation + 90) * 4 + "deg)");
+		} else $topLeft.css("display", "none");
+	}, 1);
 });
 
 function dist(x1, x2, y1, y2) {
@@ -89,6 +172,7 @@ function speedToRotation(speed) {
 }
 
 function getRotationAngle(rawCss) {
+	if (!rawCss) return NaN;
 	var values = rawCss.split('(')[1],
 	    values = values.split(')')[0],
 	    values = values.split(',');
--- js/src/main.js
+++ js/src/main.js
@@ -1,38 +1,37 @@
-$(function(){var Pointer = React.createClass({
+var Backing = React.createClass({
 	render: function(){
 		document.addEventListener('mouseup', this.onMouseUp);
 		document.addEventListener('mousedown', this.onMouseDown);
 		document.addEventListener('touchstart', this.onMouseDown);
 		document.addEventListener('touchend', this.onMouseUp);
-		return(
-	        <div className="pointer" style={this.style()}>
-	            <div className="head"></div>
-	            <div className="body"></div>
-	            <div className="tail"></div>
-	            <div className="pin"></div>
-	        </div>
-        );
-	},
-	getInitialState: function(){
-		return {
-			rotation: 0,
-			startMouseX: NaN,
-			startMouseY: NaN,
-			endMouseX: NaN,
-			endMouseY: NaN,
-			timeStart: Date.now(),
-			timeEnd: Date.now(),
-		};
-	},
-	rotate: function(rotateAmount){
-		var finalPosition = this.state.rotation + rotateAmount;
-		this.setState({rotation: finalPosition});
-	},
-	style: function(){
-		return {
-			transform: 'rotate(' + this.state.rotation + 'deg)'
-		};
-	},
+		return (    
+	<div className="backing">
+        <div className="pure-g">
+            <div className="section top-left pure-u-1-2">
+                <div className="color-circle">
+                    <i className="fa-arrow-down fa"></i>
+                </div>
+            </div>
+            <div className="section top-right pure-u-1-2">
+                <div className="color-circle">
+                    <i className="fa-arrow-down fa"></i>
+                </div>
+            </div>
+        </div>
+        <div className="pure-g">
+            <div className="section bot-left pure-u-1-2">
+                <div className="color-circle">
+                    <i className="fa-arrow-down fa"></i>
+                </div>
+            </div>
+            <div className="section bot-right pure-u-1-2">
+                <div className="color-circle">
+                    <i className="fa-arrow-down fa"></i>
+                </div>
+            </div>
+        </div>
+        <Pointer rotation={this.state.rotation}></Pointer>
+    </div>);},
 	onMouseUp: function(e){
 		if (e.type == "touchend"){
 			this.setState({
@@ -47,7 +46,6 @@ $(function(){var Pointer = React.createClass({
 				endMouseY: e.pageY,
 				timeEnd: Date.now(),
 			});
-		console.log(this.state);
 		var distance = dist(this.state.endMouseX, this.state.startMouseX, this.state.endMouseY, this.state.startMouseY);
 		var speed = getSpeed(distance, this.state.timeStart, this.state.timeEnd);
 		this.rotate(speedToRotation(speed));
@@ -66,11 +64,81 @@ $(function(){var Pointer = React.createClass({
 				startMouseY: e.pageY,
 				timeStart: Date.now(),
 			});
-	}
+	},
+	getInitialState: function(){
+		return {
+			rotation: 0,
+			startMouseX: NaN,
+			startMouseY: NaN,
+			endMouseX: NaN,
+			endMouseY: NaN,
+			timeStart: Date.now(),
+			timeEnd: Date.now(),
+		};
+	},
+	rotate: function(rotateAmount){
+		var finalPosition = this.state.rotation + rotateAmount;
+		this.setState({rotation: finalPosition});
+	},
+});
+var Pointer = React.createClass({
+	render: function(){
+		return(
+	        <div className="pointer" style={this.style()}>
+	            <div className="head"></div>
+	            <div className="body"></div>
+	            <div className="tail"></div>
+	            <div className="pin"></div>
+	        </div>
+        );
+	},
+	style: function(){
+		return {
+			transform: 'rotate(' + this.props.rotation + 'deg)'
+		};
+	},
 });
 
-var reactPointer = React.createElement(Pointer, null);
-ReactDOM.render(reactPointer, document.getElementById("pointer"));
+var reactBacking = React.createElement(Backing, null);
+ReactDOM.render(reactBacking, document.getElementById("backing"));
+
+var currentRotation = 0;
+
+$(function(){
+var $pointer = $(".pointer");
+var $topLeft = $(".top-left .fa");
+var $topRight = $(".top-right .fa");
+var $bottomRight = $(".bot-right .fa");
+var $bottomLeft = $(".bot-left .fa");
+
+var timer = setInterval(function () {
+    currentRotation = getRotationAngle($pointer.css("transform"));
+    console.log(currentRotation);
+	if (currentRotation >= 0 && currentRotation < 90){
+		$topRight.css("display", "inline-block");
+		$topRight.css("transform", "rotate(" + currentRotation * 4 + "deg)");
+	}
+	else
+		$topRight.css("display", "none");
+	if (currentRotation >= 90 && currentRotation < 180){
+		$bottomRight.css("display", "inline-block");
+		$bottomRight.css("transform", "rotate(" + (currentRotation - 90) * 4 + "deg)");
+	}
+	else
+		$bottomRight.css("display", "none");
+	if (currentRotation >= -180 && currentRotation < -90){
+		$bottomLeft.css("display", "inline-block");
+		$bottomLeft.css("transform", "rotate(" + (currentRotation + 180) * 4 + "deg)");
+	}
+	else
+		$bottomLeft.css("display", "none");
+	if (currentRotation >= -90 && currentRotation < 0){
+		$topLeft.css("display", "inline-block");
+		$topLeft.css("transform", "rotate(" + (currentRotation + 90) * 4 + "deg)");
+	}
+	else
+		$topLeft.css("display", "none");
+}, 1);
 });
 
 
@@ -91,6 +159,8 @@ function speedToRotation(speed){
 }
 
 function getRotationAngle(rawCss){
+	if (!rawCss)
+		return NaN;
 	var values = rawCss.split('(')[1],
 	    values = values.split(')')[0],
 	    values = values.split(',');
